8dd9c5860f8fb633d2ac0301e960673cb17bf197
Doman-sp-qs <122662138+Doman-sp-qs@users.noreply.github.com>
2023-02-10 11:59:03 +0000

    [Add] editページの大枠完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,39 @@
 class BooksController < ApplicationController
   def index
+    # 投稿一覧表示機能
+    @books = Book.all
+    
+    # 新規投稿機能
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
+    
+    
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+    
+  end
+  
+  
+  private
+  #　ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
